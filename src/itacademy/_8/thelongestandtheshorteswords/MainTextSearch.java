e5ca8a5e9d041308f52c7d625e22b18a88aef77e
karolkurbiel <kkurbiel@gmail.com>
2020-11-01 00:30:01 +0100

    Improved readability, getList() method divided into two ☔

--- src/itacademy/_8/thelongestandtheshorteswords/MainTextSearch.java
+++ src/itacademy/_8/thelongestandtheshorteswords/MainTextSearch.java
@@ -1,50 +1,52 @@
 package itacademy._8.thelongestandtheshorteswords;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class MainTextSearch {
 
     public static void main(String[] args) {
         String input = "For given text find the longest and the shortest words";
 
-        System.out.println("Shortest: " + getList(input, false).toString());
-        System.out.println("Longest: " + getList(input, true).toString());
+        System.out.println("Shortest: " + getShortestWords(input));
+        System.out.println("Longest: " + getLongestWords(input));
     }
 
+    private static Collection<String> getShortestWords(String input) {
+        String[] separatedString = input.split(" ");
+        int lowestNumberOfCharacters = Integer.MAX_VALUE;
+        Collection<String> shortestList = new ArrayList<>();
 
+        for(String word : separatedString) {
+            int numberOfCharacters = word.length();
 
-    private static List<String> getList(String input, boolean longest) {
-
-        String[] dictionary = input.split(" ");         //splits String into array of Strings
-
-        int sum = 0;
-        int sum1 = Integer.MAX_VALUE;                        //assigned maximum value of integer for comparison
-
-        List<String> list = new ArrayList<>();
-
-        for(String word : dictionary) {
-            int count = word.length();                      //number of characters in every word from initial String
-
-            if(longest) {                                   //checks if we are looking for longest or shortest String
-                if(count > sum) {
-                    list.clear();                           //found new shortest so previous list is no longer valid
-                    sum = count;                            //new limit for comparison
-                    list.add(word);                         //creates new list of shortest words from initial String sentence
-                } else if(count == sum) {
-                    list.add(word);                         //same number of characters as in previous comparison
-                }
-            } else {
-                if(count < sum1) {
-                    list.clear();
-                    sum1 = count;
-                    list.add(word);
-                } else if(count == sum1) {
-                    list.add(word);
-                }
+            if(numberOfCharacters < lowestNumberOfCharacters) {
+                shortestList.clear();
+                lowestNumberOfCharacters = numberOfCharacters;
+                shortestList.add(word);
+            } else if(numberOfCharacters == lowestNumberOfCharacters) {
+                shortestList.add(word);
             }
         }
+        return shortestList;
+    }
 
-        return list;
+    private static Collection<String> getLongestWords(String input) {
+        String[] separatedString = input.split(" ");
+        int largestNumberOfCharacters = 0;
+        Collection<String> longestList = new ArrayList<>();
+
+        for(String word : separatedString) {
+            int numberOfCharacters = word.length();
+
+            if(numberOfCharacters > largestNumberOfCharacters) {
+                longestList.clear();
+                largestNumberOfCharacters = numberOfCharacters;
+                longestList.add(word);
+            } else if(numberOfCharacters == largestNumberOfCharacters) {
+                longestList.add(word);
+            }
+        }
+        return longestList;
     }
 }
