0cef430a711664064ef2c8e508f4a0d4b1218380
karolkurbiel <kkurbiel@gmail.com>
2020-10-31 23:06:50 +0100

    Improved readability, changed way of showing battery status, all conditions required to charge now being processed inside charge() method, rebuild useApplication() to process all conditions required to discharge ☔

--- src/itacademy/_8/mobilephone/MobilePhone.java
+++ src/itacademy/_8/mobilephone/MobilePhone.java
@@ -8,7 +8,6 @@ public class MobilePhone {
     private final String model;
     private final int batteryCapacity;
     private int batteryAmountLeft;
-//    private final int memory;
     private int memoryLeft;
     private final List<String> applicationList;
 
@@ -16,8 +15,6 @@ public class MobilePhone {
         this.brand = brand;
         this.model = model;
         this.batteryCapacity = batteryCapacity;
-//        this.memory = memory;
-
         this.batteryAmountLeft = batteryCapacity;
         this.memoryLeft = memory;
         this.applicationList = new ArrayList<>();
@@ -25,7 +22,7 @@ public class MobilePhone {
 
     public void installApplication(String name, int size) {
         if(!applicationList.contains(name.toLowerCase())) {
-            if(install(name, size)) {
+            if(installing(name, size)) {
                 System.out.println("Application " + name + " has been installed. Remaining memory: " + memoryLeft + " MB.");
             } else {
                 System.out.println("Application " + name + " failed to install. Not enough memory!");
@@ -37,15 +34,19 @@ public class MobilePhone {
 
     public void useApplication(String name, int time) {
         if(!applicationList.contains(name.toLowerCase())) {
-            System.out.println("Application " + name + " not found in your " + this.brand + ".");
+            System.out.println("Application " + name + " not found on your " + brand + ".");
         } else {
-            int run = discharging(time);
-            if(run == 0) {
-                System.out.println("Application " + name + " has been running for " + time + " hours. Battery status: " + batteryPercent() + "%");
-            } else if(run == -1) {
+            int demandedCapacity = time * 100;
+            if(batteryAmountLeft > demandedCapacity) {
+                discharging(demandedCapacity);
+                System.out.println("Application " + name + " has been running for " + time + " hours. Remaining battery capacity: " + batteryAmountLeft + " mAh");
+            } else if(batteryAmountLeft == demandedCapacity) {
+                discharging(demandedCapacity);
                 System.out.println("Application " + name + " has been running for " + time + " hours. Battery depleted.");
-            } else if(run > 0){
-                System.out.println("Application " + name + " has been running only for " + (time - run) + " hours. Battery depleted.");
+            } else if(batteryAmountLeft > 0) {
+                int timeLeft = time - (batteryAmountLeft / 100);
+                discharging(batteryAmountLeft);
+                System.out.println("Application " + name + " has been running only for " + (time - timeLeft) + " hours. Battery depleted.");
             } else {
                 System.out.println("Application " + name + " failed to run. Battery depleted!");
             }
@@ -53,57 +54,23 @@ public class MobilePhone {
     }
 
     public void charge() {
-        if(charging()) {
-            System.out.println(brand + " " + model + " has been fully charged. Battery status: " + batteryPercent() + "%");
-        } else {
-            System.out.println(brand + " " + model + " is fully charged. Battery status: " + batteryPercent() + "%");
-        }
-    }
-
-    private int discharging(int time) {
-        // returns 0  : application runtime completed, battery capacity updated
-        // returns -1 : application runtime completed, battery discharged
-        // returns -2 : application cannot be run, battery depleted.
-        // returns >0 : application runtime not completed, hours left to complete
-
-        if(batteryAmountLeft == 0) {
-            return -2;
-        }
-
-        int amountToConsume = time * 100;
-        if(this.batteryAmountLeft > amountToConsume) {
-            this.batteryAmountLeft -= amountToConsume;
-            return 0;
-        }
-
-        int hoursLeft = batteryAmountLeft / 100;
-        this.batteryAmountLeft = 0;
-        if(time == hoursLeft) {
-            return -1;
-        } else {
-            return time - hoursLeft;
-        }
-    }
-
-    private int batteryPercent() {
-        // returns percentage of battery
-
-        return ((100*batteryAmountLeft)/batteryCapacity);
-    }
-
-    private boolean charging() {
-        // updates batteryAmount to its initial condition specified in batteryCapacity
-
         if(batteryCapacity != batteryAmountLeft) {
-            this.batteryAmountLeft = this.batteryCapacity;
-            return true;
+            charging();
+            System.out.println(brand + " " + model + " has been fully charged. Remaining battery capacity: " + batteryAmountLeft + " mAh");
+        } else {
+            System.out.println(brand + " " + model + " is fully charged. Remaining battery capacity: " + batteryAmountLeft + " mAh");
         }
-        return false;
     }
 
-    private boolean install(String name, int size) {
-        // updates memoryLeft after "installation"
+    private void discharging(int amount) {
+        this.batteryAmountLeft -= amount;
+    }
 
+    private void charging() {
+        this.batteryAmountLeft = this.batteryCapacity;
+    }
+
+    private boolean installing(String name, int size) {
         if(memoryLeft >= size) {
             this.memoryLeft -= size;
             this.applicationList.add(name.toLowerCase());
